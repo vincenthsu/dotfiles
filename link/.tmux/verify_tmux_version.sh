2fee1e82bf6625ee1808a1150188099e53d8c475
Vincent Hsu <vincent.hsu@hotmail.com>
2016-05-04 16:51:13 +0800

    Correct loading TMUX plugin manager

--- link/.tmux/verify_tmux_version.sh
+++ link/.tmux/verify_tmux_version.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 verify_tmux_verision () {
-    tmux_home=~/.tmux
+    tmux_home=$HOME/.tmux
     tmux_version="$(tmux -V | cut -c 6-)"
 
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_2.1_up.conf"
+        tmux run-shell "$tmux_home/plugins/tpm/tpm"
         exit
     elif [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_1.9_to_2.1.conf"
+        tmux run-shell "$tmux_home/plugins/tpm/tpm"
         exit
     else
-        echo "1.8-"
         tmux source-file "$tmux_home/tmux_1.9_down.conf"
         exit
     fi
